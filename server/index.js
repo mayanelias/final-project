90b81a60eec72c6d3655d8b8e3926f2924d8e974
mayanelias <eliasmayan@gmail.com>
2022-02-14 17:22:50 +0200

    heroko

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ console.log("app is loading");
 import {addUser,getConcertsData,updateWishList,getUserById,deleteFromWishList, addContact} from "./utils.js";
 import express from "express";
 const app = express();
+import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 app.use(express.json());
 // app.get("/concerts", (req, res) => {
 // insertConcertsData(req,res);
@@ -23,6 +28,10 @@ app.get("/users/:id", (req, res) => {
 });
 app.patch("/users/delete/:id", (req, res) => {
   deleteFromWishList(req, res);
+});
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, resp) => {
+  resp.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
